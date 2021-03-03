a1f859b9c6d36e6b9a7dc3c812405c1cbf2b85df
zhoufei <summergtr@163.com>
2021-03-03 11:17:11 +0800

    111

---
+++ test_api/src/main/java/com/summer/domain/Student.java
@@ -0,0 +1,13 @@
+package com.summer.domain;
+
+/**
+ * @ClassName Student
+ * @Description
+ * @Author summer
+ * @Date 2021/3/3 11:10
+ **/
+public class Student {
+    private String name;
+    private Integer id;
+    private String idcard;
+}
